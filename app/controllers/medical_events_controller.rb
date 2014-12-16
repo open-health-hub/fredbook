5af44fb8aad69fb3854a9c234e7e2b8b287af599
Prelang Builder <builder@prelang.com>
2014-12-16 12:43:56 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/medical_events_controller.rb
+++ app/controllers/medical_events_controller.rb
@@ -1,4 +1,8 @@
 class MedicalEventsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_medical_event, only: [:show, :edit, :update, :destroy]
 
   # GET /medical_events
